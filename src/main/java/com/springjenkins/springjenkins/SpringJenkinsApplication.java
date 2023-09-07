5cf42620ca94af77d1789d9c074739d20882d12a
shivam-nitm <shivam.singh.nitm@gmail.com>
2023-09-08 00:37:17 +0530

    added code to test jenkins

--- src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
@@ -1,13 +1,26 @@
 package com.springjenkins.springjenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringJenkinsApplication {
+	
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
+		
+		testJenkinsBuild();
+	}
+	
+	public static void testJenkinsBuild() {
+		logger.info("Application started runnint, inside the testJenkinsBuild method");
+		System.out.println("testing jenkins");
 	}
 
 }
--- src/test/java/com/springjenkins/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/springjenkins/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.springjenkins.springjenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("inside SpringJenkinsApplicationTests");
 	}
 
 }
