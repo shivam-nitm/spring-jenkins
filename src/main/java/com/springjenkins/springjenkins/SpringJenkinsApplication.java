47a7eabbc681662732c1d0464c838d1661ef1117
shivam-nitm <shivam.singh.nitm@gmail.com>
2023-09-08 03:18:42 +0530

    fixed the code

--- src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
@@ -18,7 +18,7 @@ public class SpringJenkinsApplication {
 		testJenkinsBuild();
 	}
 	
-	public static  testJenkinsBuild() {
+	public static void testJenkinsBuild() {
 		logger.info("Application started runnint, inside the testJenkinsBuild method");
 		System.out.println("testing jenkins");
 	}
